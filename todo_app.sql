cad89344a8dec37cb9f07cc360a8832f5b345007
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-12 15:40:19 -1000

    Table Altering and Updating Tasks

--- todo_app.sql
+++ todo_app.sql
@@ -1,20 +1,27 @@
 --1. Write a query to drop a database named `todo_app` if it exists
+
 DROP DATABASE IF EXISTS todo_app;
 
 --1. Write a query to delete a user named `michael` if it exists
--- DELETE FROM users
--- WHERE EXISTS user = 'michael'
+
+DROP USER IF EXISTS michael;
 
 --1. Write a query to create a user named `michael` with an encrypted password `stonebreaker`
--- INSERT INTO users (first_name, password)
--- VALUES ('michael', 'stonebreaker')
+
+CREATE USER michael
+WITH ENCRYPTED PASSWORD 'sqlsucks';
 
 --1. Write a query to create a database named `todo_app`
+
 CREATE DATABASE todo_app;
 
 -- 1. Connect to the newly created database
 
--- 1. Write a query to create a table named `tasks` using the **Initial columns** detailed below
+\c todo_app;
+
+-- 1. Write a query to create a table named `tasks` using the
+-- **Initial columns** detailed below
+
 CREATE TABLE tasks(
     id          SERIAL         NOT NULL PRIMARY KEY,
     title       VARCHAR(255)   NOT NULL,
@@ -26,10 +33,32 @@ CREATE TABLE tasks(
 );
 
 
---1. Define column `id` as the tables primary key
-
-
 --1. Write queries to accomplish the following
 --  1. remove the column named `completed`
---  1. add a column to `tasks` named `completed_at`:timestamp, that may be NULL, and has a default value of `NULL`.
---  1. change the `updated_at` column to not allow NULL values, and have a default value of `now()`
+--  1. add a column to `tasks` named `completed_at`:timestamp,
+--  that may be NULL, and has a default value of `NULL`.
+--  1. change the `updated_at` column to not allow NULL values,
+--  and have a default value of `now()`
+
+ALTER TABLE tasks
+  DROP COLUMN completed,
+  ADD COLUMN completed_at TIMESTAMP NULL DEFAULT NULL,
+  ALTER COLUMN updated_at SET NOT NULL,
+  ALTER COLUMN updated_at SET DEFAULT now();
+
+
+-- 1. create a new task, by only setting values (not defining which columns)
+--   `id = default value`
+--   `title = 'Study SQL'`
+--   `description = 'Complete this exercise'`
+--   `created_at = now()`
+--   `updated_at = now()`
+--   `completed_at = NULL`
+
+UPDATE tasks
+  SET id = default,
+      title = 'Study SQL',
+      description = 'Complete this exercise',
+      created_at = now(),
+      updated_at = now(),
+      completed_at = NULL;
